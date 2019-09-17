31a0abdcb112bbdd644496806969499e52ab4cdb
LuisT <ltaborda0615@gmail.com>
2019-09-16 20:36:35 -0400

    bug fixes

---
+++ app/data/friends.js
@@ -0,0 +1,82 @@
+var friends = [
+    {
+        "name":"Marcy",
+        "photo": "https://st3.depositphotos.com/4421371/15483/i/1600/depositphotos_154837522-stock-photo-teenage-girl-with-bob-haircut.jpg",
+        "score" : [
+            2,
+            4,
+            5,
+            5,
+            3,
+            1,
+            2,
+            3,
+            3,
+            1
+        ]
+    },
+    {
+        "name":"Steve",
+        "photo":"https://www.dermabrightclinic.com/wp-content/uploads/2019/02/men-facial.jpeg",
+        "score": [
+            4,
+            2,
+            5,
+            3,
+            3,
+            1,
+            4,
+            2,
+            1,
+            1
+        ]
+    },
+    {
+        "name":"Jennifer",
+        "photo":"https://pbs.twimg.com/profile_images/1019006679097020416/p2yMQV5J_400x400.jpg",
+        "score" : [
+            2,
+            4,
+            5,
+            4,
+            3,
+            3,
+            2,
+            1,
+            4,
+            3
+        ]
+    },
+    {
+        "name":"Daryll",
+        "photo":"https://miro.medium.com/max/1200/1*pu-_w1ikRyESKk4VLRaNfA.png",
+        "score": [
+            1,
+            4,
+            5,
+            5,
+            2,
+            4,
+            3,
+            1,
+            5,
+            2
+        ]
+    },
+    {
+        "name": "May",
+        "photo": "https://d2ck0sxsjau14o.cloudfront.net/wp-content/uploads/2016/01/Asian-teen-girl-by-Angelin-Song.jpg",
+        "score": [
+            3,
+            3,
+            4,
+            5,
+            1,
+            2,
+            4,
+            3,
+            1,
+            3
+        ]
+    }
+]
